5d729caff65bb4a2de50a84ef9d7807018083ca8
meddaz <40867923+meddaz@users.noreply.github.com>
2023-12-11 18:28:42 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
 function subscribe() {
-  function isValidEmail(email) {
-      return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    function isValidEmail(email) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    }
+  
+    var userEmail = document.getElementById("signupForm").value;
+    var card = document.getElementById("card");
+    var error = document.getElementById("error");
+  
+    if (isValidEmail(userEmail)) {
+  
+        card.innerHTML = `
+            <img src='assets/images/icon-success.svg' id='successIcon' />
+            <h1>Thanks for subscribing!</h1>
+            <p>A confirmation has been sent to ${userEmail}. Please open it and click the button inside to confirm your subscription.</p>
+            <br>
+            <button id="dismissMsg" onclick="dismiss()">Dismiss message</button>
+        `;
+  
+        card.style.width = '504px';
+        card.style.height = '520px';
+        card.style.padding = '90px';
+  
+        var image = document.getElementById("signup");
+        image.remove();
+    } else {
+        document.getElementById("signupForm").style.backgroundColor = 'rgba(255, 98, 87, 0.5)';
+        document.getElementById("signupForm").style.color = "#FF6155";
+        document.getElementById("signupForm").style.fontWeight = "bold";
+        document.getElementById("signupForm").style.fontSize = "12px";
+  
+        error.innerHTML = "Valid email required";
+  
+    }
   }
+  
+  function resize() {
 
-  var userEmail = document.getElementById("signupForm").value;
-  var card = document.getElementById("card");
-  var error = document.getElementById("error");
+    if (window.innerWidth < 700) {
 
-  if (isValidEmail(userEmail)) {
-
-      card.innerHTML = `
-          <img src='icon-success.svg' id='successIcon' />
-          <h1>Thanks for subscribing!</h1>
-          <p>A confirmation has been sent to ${userEmail}. Please open it and click the button inside to confirm your subscription.</p>
-          <br>
-          <button id="dismissMsg" onclick="dismiss()">Dismiss message</button>
-      `;
-
-      card.style.width = '504px';
-      card.style.height = '520px';
-      card.style.padding = '90px';
-
-      var image = document.getElementById("signup");
-      image.remove();
-  } else {
-      document.getElementById("signupForm").style.backgroundColor = 'rgba(255, 98, 87, 0.5)';
-      document.getElementById("signupForm").style.color = "#FF6155";
-      document.getElementById("signupForm").style.fontWeight = "bold";
-      document.getElementById("signupForm").style.fontSize = "12px";
-
-      error.innerHTML = "Valid email required";
-
-  }
+     document.getElementById("signup").innerHTML  = <img src="assets\images\illustration-sign-up-mobile.svg" id="mobileSignup"></img>
+    }
 }
