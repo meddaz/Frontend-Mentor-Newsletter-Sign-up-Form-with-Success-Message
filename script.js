da70370db2d843dc5e639c1d2650be628c759b9c
meddaz <40867923+meddaz@users.noreply.github.com>
2023-12-18 11:19:08 -0800

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ function subscribe() {
     const userEmail = document.getElementById("signupForm").value;
     const card = document.getElementById("card");
     const error = document.getElementById("error");
+const mobileImg = document.getElementByClassName("mobile-img");  
     if (isValidEmail(userEmail)) {
-
+  
         card.innerHTML = `
             <img src='icon-success.svg' id='successIcon' />
             <h1>Thanks for subscribing!</h1>
@@ -19,21 +20,26 @@ function subscribe() {
         card.style.width = '504px';
         card.style.height = '520px';
         card.style.padding = '90px';
-        const immage = document.getElementById("signup");
-
+  
         image.remove();
     } else {
-
         document.getElementById("signupForm").style.backgroundColor = 'rgba(255, 98, 87, 0.5)';
         document.getElementById("signupForm").style.color = "#FF6155";
         document.getElementById("signupForm").style.fontWeight = "bold";
         document.getElementById("signupForm").style.fontSize = "12px";
-
-        const mobileImg = document.getElementsByClassName("mobile-img");  
   
         error.innerHTML = "Valid email required";
    mobileImg.remove();
-            image.remove();
-}
+    }
   }
   
+  function resize() {
+const signupElement = document.getElemmentById("signup");
+    if (window.innerWidth < 700) {
+
+     signupElement.innerHTML  = `<img src="assets\images\illustration-sign-up-mobile.svg" id="mobileSignup">`
+    }
+    else {
+        signupElement.innerHTML = ''; // or set it to some default value
+    }
+}
